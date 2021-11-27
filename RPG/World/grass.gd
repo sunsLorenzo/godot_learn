ffeaa20bf1984966dc9577de9e6029e0c95508ff
sun <2213528663@qq.com>
2021-11-27 23:19:57 +0800

    ep11. hit box and hurt box &collision layers

--- RPG/World/grass.gd
+++ RPG/World/grass.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+#
+#func _process(delta):
+	#if Input.is_action_pressed("attack"):
 
-func _process(delta):
-	if Input.is_action_pressed("attack"):
-		var grassEffect = load("res://Effects/grassEffect.tscn").instance()
-		
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position # global_position is the position of the grass
-		queue_free()
+func creat_grass_effect():
+	var grassEffect = load("res://Effects/grassEffect.tscn").instance()
+	
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position # global_position is the position of the grass
+	queue_free()
+
+
+func _on_Hurtbox_area_entered(area):
+	creat_grass_effect()
+	queue_free()
