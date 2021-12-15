1a41956bbb711a7f0f5c52697dd4ccec01960e75
sunsLorenzo <2213528663@qq.com>
2021-12-15 17:51:59 +0800

    bat wander finish

--- RPG/Enemies/Bat.gd
+++ RPG/Enemies/Bat.gd
@@ -6,7 +6,7 @@ enum{
 	WANDER,
 	CHASE
 }
-export var ACCELARATION = 100
+export var ACCELARATION = 200
 export var MAX_SPPED = 50
 export var FRICTION = 400
 
@@ -23,33 +23,41 @@ onready var wanderController = $WanderController
 
 
 func _ready():
+	randomize()
 	print(stats.max_health)
 	print(stats.health)
-	
+	update_wander()
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 300*delta)
 	knockback = move_and_slide(knockback)
 	
 	match state:
 		IDLE:
-			velocity =  velocity.move_toward(Vector2.ZERO, FRICTION*delta)
+			velocity =  velocity.move_toward(Vector2.ZERO, FRICTION)
 			seek_player()
-		WANDER:
 			if wanderController.get_time_left() == 0:
-				pass
-#			velocity.x = rand_range(-1,1)
-#			velocity.y = rand_range(-1,1)
-#			velocity = velocity.normalized()*MAX_SPPED*0.3
-# #			velocity = velocity.move_toward(velocity*MAX_SPPED, ACCELARATION*delta)
-			state=IDLE
+				update_wander()
+			
+		WANDER:
+			seek_player()
+#			wanderController.update_target_position()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+#				wanderController.update_target_position()
+			accelerate_towards( wanderController.target_position, delta)
+			#新的wander state
+#			if global_position.distance_to(wanderController.target_position)<=1:
+#				update_wander()
+		
 		CHASE:
 			var player = playerDetectionZone.player
 			if player!=null:
-				var direction = (player.global_position -global_position).normalized()
-				velocity = velocity.move_toward(direction*MAX_SPPED, ACCELARATION*delta)
+#				var direction = (player.global_position -global_position).normalized()
+				accelerate_towards( player.global_position, delta)
 			else:
-				state=WANDER
-			sprite.flip_h = velocity.x<0
+				state=IDLE
+			
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector()*delta*MAX_SPPED*3
 		 
@@ -60,12 +68,20 @@ func seek_player():
 		state = CHASE
 	else:
 		state = WANDER
+func update_wander():
+	state = pick_random_state([IDLE,WANDER])
+	wanderController.set_wander_timer(rand_range(1,3))
 
 func pick_random_state(state_list):
 	state_list.shuffle()
 	return state_list.pop_front()
 
-
+func accelerate_towards( point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction*MAX_SPPED, ACCELARATION*delta)
+	sprite.flip_h = velocity.x<0
+	
+	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 #	if stats.health <= 0:
--- RPG/Enemies/WanderController.gd
+++ RPG/Enemies/WanderController.gd
@@ -2,14 +2,24 @@ extends Node2D
 
 export(int) var wander_range = 32
 onready var start_posititon = global_position
-onready var target_position = global_position
+onready var target_position = global_position+Vector2(rand_range(-wander_range,wander_range), rand_range(-wander_range,wander_range))
 onready var timer = $Timer
 
 func _on_Timer_timeout():
 	update_target_position()
-	
+	print(target_position)
+
+
 func update_target_position():
 	var target_vector = Vector2(rand_range(-wander_range,wander_range), rand_range(-wander_range,wander_range))
-	target_position = start_posititon+target_position
+	target_position = start_posititon + target_vector
+#	start_posititon = global_position
+
+
+func set_wander_timer(duration):
+	timer.start(duration)
+	print('set', duration)
+
+
 func get_time_left():
 	return timer.time_left
